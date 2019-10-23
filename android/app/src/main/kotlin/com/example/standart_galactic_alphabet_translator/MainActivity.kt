eb0c93d24622b7f2ca183642631ea384e476f099
ghostsshadow <ghostsshadow@users.noreply.github.com>
2019-10-24 00:03:23 +0200

    first commmit

---
+++ android/app/src/main/kotlin/com/example/standart_galactic_alphabet_translator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.standart_galactic_alphabet_translator
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
